25456b58687fd6d3393a92e584bafaa29e219841
nestrockx <wegielek.art@gmail.com>
2025-05-29 11:54:01 +0200

    Add more room types

--- simple_planning_poker/models/room.py
+++ simple_planning_poker/models/room.py
@@ -9,7 +9,7 @@ def generate_code():
 class Room(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=6, unique=True, default=generate_code)
-    type = models.CharField(max_length=20, choices=[('default', 'Default'), ('fibonacci', 'Fibonacci')], default='default')
+    type = models.CharField(max_length=20, choices=[('default', 'Default'), ('fibonacci', 'Fibonacci'), ('tshirts', 'Tshirts'), ('powers', 'Powers')], default='default')
     participants = models.ManyToManyField(User, related_name='joined_rooms', blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms')
     created_at = models.DateTimeField(auto_now_add=True)
