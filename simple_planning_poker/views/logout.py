92e2e0eeec36eb293b9cc3ef56e259bdbcb7bcc7
nestrockx <wegielek.art@gmail.com>
2025-06-09 20:29:59 +0200

    Revert "Adjust login for android"
    This reverts commit eb3b5823196fd72cf50ba423ccca6cd7e9a0b4fc.

--- simple_planning_poker/views/logout.py
+++ simple_planning_poker/views/logout.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response
+from django.http import HttpResponse
 from rest_framework.views import APIView
 
 from rest_framework.permissions import AllowAny
@@ -7,7 +7,7 @@ class LogoutView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        response = Response(status=204)
+        response = HttpResponse(status=204)
         response.delete_cookie(
             key = "accessToken",
             samesite="Lax",
